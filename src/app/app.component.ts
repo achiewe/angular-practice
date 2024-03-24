8739bb8ea91c3260fc3e0acd03e3fbc0efd3d4ef
achiewe <mamukashviliachi706@gmail.com>
2024-03-24 13:48:33 +0400

    create in the app component data object array which is object array and then to show it in the app component html use structure directive ngFor and ngIf and also dont use html element to render beucause it was excess and use for that ng-container

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 
+interface Data {
+  id: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'achi-angular-app';
+  public data: Data[] = [
+    { id: '1', name: 'achi' },
+    { id: '2', name: 'gena' },
+    { id: '3', name: 'cxela' },
+  ];
 }
