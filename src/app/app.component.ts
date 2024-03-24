ea4ec3f0f05d5821b0b7eceaf40687b72d3cc2b3
achiewe <mamukashviliachi706@gmail.com>
2024-03-24 14:00:55 +0400

    use *ngFor structure directive to show data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,10 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 
-interface Data {
-  id: string;
-  name: string;
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  public data: Data[] = [
-    { id: '1', name: 'achi' },
-    { id: '2', name: 'gena' },
-    { id: '3', name: 'cxela' },
-  ];
+  public countries: string[] = ['georgia', 'usa', 'france', 'italy'];
 }
