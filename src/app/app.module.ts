13beba9268670a1ba587c9a1a05545a97b540215
achiewe <mamukashviliachi706@gmail.com>
2024-03-25 12:21:18 +0400

    style inputFilter component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { InputFilterComponent } from './input-filter/input-filter.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, InputFilterComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/input-filter/input-filter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-filter',
+  templateUrl: './input-filter.component.html',
+  styleUrl: './input-filter.component.scss'
+})
+export class InputFilterComponent {
+
+}
